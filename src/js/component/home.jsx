cd075b15ee83875ecc562d3e8aec480810318bc1
claudiareyesjofre <claudia.reyes.joffre@gmail.com>
2023-02-27 18:12:53 +0000

    diseño

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import "../../../styles/style Home/home.css";
+import { Card } from "./my-card";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const CardHome = () => {
+  const carouselRef = useRef(null);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handlePrev = () => {
+    const carousel = carouselRef.current;
+    const width = carousel.clientWidth;
+    carousel.scrollLeft -= width;
+  };
+
+  const handleNext = () => {
+    const carousel = carouselRef.current;
+    const width = carousel.clientWidth;
+    carousel.scrollLeft += width;
+  };
+
+  return (
+    <div className="product-carousel-home">
+      <button className="pre-btn" onClick={handlePrev}>
+        <p className="slider-home">
+          <FaChevronLeft />
+        </p>
+      </button>
+      <button className="next-btn" onClick={handleNext}>
+        <p className="slider-home">
+          <FaChevronRight />
+        </p>
+      </button>
+
+      <div className="container carousel-home" ref={carouselRef}>
+        <Card cardno="1" />
+        <Card cardno="2" />
+        <Card cardno="3" />
+        <Card cardno="4" />
+        <Card cardno="5" />
+        <Card cardno="6" />
+        <Card cardno="7" />
+        <Card cardno="8" />
+      </div>
+    </div>
+  );
 };
-
-export default Home;
---
+++ src/js/component/my-card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import React from "react";
+import "../../../styles/style Home/home.css"
+
+export const Card =(props) =>{
+   
+   return(
+    <div className="my-card">
+    Mycard No. {props.cardno}
+    </div>
+    ) 
+}
